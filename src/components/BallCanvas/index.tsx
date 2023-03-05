0e68143a7881eeebc17fef306777a52a11658770
Niv Stibel <Nivsvb@gmail.com>
2023-03-05 12:55:30 +0200

    remove some unused code

--- src/components/BallCanvas/index.tsx
+++ src/components/BallCanvas/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unknown-property */
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import {
     Decal,
@@ -16,38 +16,6 @@ const Ball = ({ icon }: IBallCanvasProps) => {
 
     return (
         <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-            <ambientLight intensity={0.25} />
-            <directionalLight position={[0, 0, 0.05]} />
-            <mesh castShadow receiveShadow scale={2.75}>
-                <icosahedronGeometry args={[1, 1]} />
-                <meshStandardMaterial
-                    color="#fff8eb"
-                    polygonOffset
-                    polygonOffsetFactor={-5}
-                    flatShading
-                />
-                <Decal
-                    position={[0, 0, 1]}
-                    rotation={[2 * Math.PI, 0, 6.25]}
-                    scale={1}
-                    map={decal}
-                    flatShading
-                />
-            </mesh>
-        </Float>
-    )
-}
-
-const Ball2 = ({ icon }: IBallCanvasProps) => {
-    const [decal] = useTexture([icon])
-
-    return (
-        <Float
-            position={[2, 3, 0]}
-            speed={1.75}
-            rotationIntensity={1}
-            floatIntensity={2}
-        >
             <ambientLight intensity={0.25} />
             <directionalLight position={[0, 0, 0.05]} />
             <mesh castShadow receiveShadow scale={2.75}>
