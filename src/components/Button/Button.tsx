1e14fc1bc4956ba2cf9e09df6756c13ee173239f
Niv Stibel <Nivsvb@gmail.com>
2023-04-28 22:44:40 +0300

    removed unused code

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,40 @@
 import clsx from 'clsx'
+import tw from 'tailwind-styled-components'
 interface Props {
     children: React.ReactNode
-    className?: string
     onClick?: () => void
 }
 
+const ButtonStyle = tw.div`
+    flex
+    border
+    rounded-md
+    border-1
+    border-solid
+    border-callToAction
+    px-3
+    py-2
+    text-callToAction
+    transition-all
+    duration-500
+    hover:bg-opacity-25
+    hover:bg-callToAction
+    w-fit
+    justify-center
+    items-center
+    text-center
+`
+
 const Button = (props: Props) => {
     return (
-        <div
+        <ButtonStyle
             onClick={props.onClick}
             role="button"
             tabIndex={0}
             onKeyDown={props.onClick}
-            className={clsx(
-                'flex border rounded-md border-1 border-solid border-callToAction px-3 py-2 text-callToAction transition-all duration-500 hover:bg-opacity-25 hover:bg-callToAction w-fit justify-center items-center text-center',
-                props.className
-            )}
         >
             {props.children}
-        </div>
+        </ButtonStyle>
     )
 }
 
--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -28,28 +28,6 @@ const Hero = () => {
                     </p>
                 </div>
             </div>
-            {/* <div className="flex items-start justify-center p-0 flex-col mx-[150px]">
-                <p className="text-callToAction mb-2">Hi, my name is</p>
-                <p className="text-secondary text-6xl mb-2">Niv Shtibel.</p>
-                <p className="text-primary text-6xl mb-2">
-                    I build things for the web.
-                </p>
-                <div className="text-primary max-w-[540px] mb-6">
-                    I’m a full-stack developer specializing in the backend.
-                    Currently, I’m focused on building shopify apps, web
-                    products at{' '}
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://oversight.co.il/"
-                        className="inline text-callToAction hover:"
-                    >
-                        Oversight
-                    </a>
-                    .
-                </div>
-                <Button className="w-36 h-12">Get in touch</Button>
-            </div> */}
             <ComputerCanvas />
             <div className="absolute bottom-[10%] md:bottom-[10%] w-full flex justify-center items-center">
                 <a href="#about">
