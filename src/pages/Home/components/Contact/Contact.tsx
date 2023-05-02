97b3923bd390337a447860190a1f9da05aa9969b
Niv Stibel <Nivsvb@gmail.com>
2023-05-02 23:54:05 +0300

    fix contact

--- src/pages/Home/components/Contact/Contact.tsx
+++ src/pages/Home/components/Contact/Contact.tsx
@@ -79,9 +79,6 @@ const Contact = () => {
                     <p className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
                         Get in touch
                     </p>
-                    <h3 className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
-                        Contact.
-                    </h3>
 
                     <form
                         ref={formRef}
