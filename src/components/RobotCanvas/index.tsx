697f711c0c23a92402b42bf286064409079734fd
Niv Stibel <Nivsvb@gmail.com>
2023-05-04 09:18:23 +0300

    added glow to robot

--- src/components/RobotCanvas/index.tsx
+++ src/components/RobotCanvas/index.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/no-unknown-property */
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { Bloom, EffectComposer } from '@react-three/postprocessing'
+import { BlendFunction } from 'postprocessing'
 
 import CanvasLoader from '../CanvasLoader'
 
@@ -32,21 +36,24 @@ const RobotCanvas = () => {
                 position: [-4, 3, 5],
             }}
         >
-            <ambientLight intensity={10} position={[100, 5, 1]} />
-            <directionalLight position={[100, 5, 1]} intensity={20} />
-            <directionalLight position={[-10, -10, -5]} intensity={10} />
-            <Suspense fallback={<CanvasLoader />}>
-                <OrbitControls
-                    autoRotate
-                    enablePan={true}
-                    enableZoom={false}
-                    maxPolarAngle={Math.PI / 2}
-                    minPolarAngle={Math.PI / 2}
-                />
-                <Robot />
+            <EffectComposer>
+                <ambientLight intensity={10} position={[100, 5, 1]} />
+                <directionalLight position={[100, 5, 1]} intensity={1} />
+                <directionalLight position={[-10, -10, -5]} intensity={1} />
+                <Suspense fallback={<CanvasLoader />}>
+                    <OrbitControls
+                        autoRotate
+                        enablePan={true}
+                        enableZoom={false}
+                        maxPolarAngle={Math.PI / 2}
+                        minPolarAngle={Math.PI / 2}
+                    />
+                    <Robot />
 
-                <Preload all />
-            </Suspense>
+                    <Preload all />
+                </Suspense>
+                <Bloom blendFunction={BlendFunction.ALPHA} />
+            </EffectComposer>
         </Canvas>
     )
 }
